d198df41e2082df2c9e0f31471fcbaa46e6a8015
Jordan Anderson <tattooguy68w@gmail.com>
2023-11-06 13:20:43 -0500

    updated projects section

--- js/darkmode.js
+++ js/darkmode.js
@@ -2,12 +2,13 @@ const body = document.querySelector("body");
 const modeToggle = document.getElementById("mode-toggle");
 const modeStatus = document.querySelector(".mode-status");
 const cardColor = document.getElementById("cardId");
-const cardColor2 = document.getElementById("cardId2")
-
+const cardColor2 = document.getElementById("cardId2");
+const cardColor3 = document.getElementById("cardId3");
 function toggleMode() {
     body.classList.toggle("dark-mode");
     cardColor.classList.toggle("cardcolor");
     cardColor2.classList.toggle("cardcolor");
+    cardColor3.classList.toggle("cardcolor");
 
     const modeMessage = body.classList.contains("dark-mode") ?
         "Dark"
